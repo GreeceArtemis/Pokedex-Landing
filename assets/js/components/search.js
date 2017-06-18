534572e86abec4acd0a397b40f109dde04c1a3db
GreeceArtemis <greece.rapu@gmail.com>
2017-06-18 01:20:56 -0500

    datos

--- assets/js/components/search.js
+++ assets/js/components/search.js
@@ -21,7 +21,7 @@ const Search = (update) => {
       img:null,
       name:jsonPokedex.pokemon_species.name,
       number:jsonPokedex.entry_number,
-      caracteristicas:null,
+      //caracteristicas:null,
       peso:null,
       sexo:null,
       categoria:null,
@@ -48,14 +48,36 @@ const Search = (update) => {
     divImg.append(icons);
     divImg.append(name);
     pokemons.append(divImg);
-    spanHeart.on('click',function(){
+
+    $.getJSON("http://pokeapi.co/api/v2/pokemon-species/"+pokemon.number+"/",function(modalPokemon){
+      const idiomaEs=4;//De esta manera eligo el objeto donde se encuentra en español
+      pokemon.caracteristicas=modalPokemon.flavor_text_entries[3].flavor_text;
+      const a=modalPokemon;
+      console.log(pokemon.caracteristicas);
+    });
+    spanPokebola.on('click',function(){
       console.log('click');
+
       //alert('click');
       $("#myModal").append(Modal(pokemon));
       //$('#myModal').on('shown.bs.modal', function () {
        //$('#myInput').focus()
     //  })
     });
+
+    $.getJSON("http://pokeapi.co/api/v2/pokemon/"+pokemon.number+"/",function(modalPokemon){
+      const idiomaEs=4;//De esta manera eligo el objeto donde se encuentra en español
+      pokemon.abilities = modalPokemon.abilities;
+      console.log(pokemon.abilities);
+      pokemon.altura = modalPokemon.height + ' m';
+      console.log(pokemon.height);
+      pokemon.peso= modalPokemon.weight + ' kg';
+      console.log(pokemon.weight);
+      pokemon.types = modalPokemon.types;
+      console.log(pokemon.types);
+    });
+
+
   }
 
 
@@ -94,35 +116,39 @@ const Search = (update) => {
 return searchContainer;
 }
 const Modal= (pokemon)=>{
-  $.getJSON("http://pokeapi.co/api/v2/pokemon-species/"+pokemon.number+"/",function(modalPokemon){
-    const idiomaEs=4;//De esta manera eligo el objeto donde se encuentra en español
-    pokemon.caracteristicas=modalPokemon.flavor_text_entries[3].flavor_text;
-    const a=modalPokemon;
-    console.log(pokemon.caracteristicas);
-  });
 
+
+  console.log(pokemon);
   const modalDialog = $('<div class="modal-dialog" role="document"></div>');
   const modalContent = $('<div class="modal-content"></div>');
   const modalHeader = $('<div class="modal-header"></div>');
   const buttonclose = $('<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>');
   const spanClose = $('<span aria-hidden="true">&times;</span>');
-  const namePokemon = $('<h2 class="modal-title" id="myModalLabel">'+pokemon.caracteristicas+'</h2>');
+  const namePokemon = $('<h2 class="modal-title" id="myModalLabel">'+pokemon.name+'</h2>');
   const modalBody = $('<div class="modal-body"></div>');
-
   const infoPokemon = $('<div class="modal-info-pokemon"></div>');
 
 
-  const spanInfo=$('<span aria-hidden="true">'+pokemon.caracteristicas+'</span>');
   const modalImgPokemon = $('<div class="modal-img-pokemon"></div>');
   const descripcionModal = $('<div class="modal-descripcion-pokemon"></div>');
+  const spanInfo=$('<span aria-hidden="true">'+pokemon.caracteristicas+'</span>');
   const caracModal = $('<div class="modal-caracteristicas-pokemon"></div>');
+    const altura=$('<p><span>Altura:<br><span>'+pokemon.altura+'</span></span></p>');
+    const peso=$('<p><span>Peso:<br><span>'+pokemon.peso+'</span></span></p>');
+    const sexo=$("<p><span>Sexo:<br><span>"+pokemon.sexo+"</span></span></p>");
+    caracModal.append(altura);
+    caracModal.append(peso);
+    caracModal.append(sexo);
+
   const tipoModal = $('<div class="modal-tipo-pokemon"></div>');
   const debilidadModal = $('<div class="modal-debilidad-pokemon"></div>');
 
-
-
-
-
+  descripcionModal.append(debilidadModal);
+  descripcionModal.append(tipoModal);
+  descripcionModal.append(caracModal);
+  descripcionModal.append(spanInfo);
+  modalBody.append(modalImgPokemon);
+  modalBody.append(descripcionModal);
 
   buttonclose.append(spanClose);
   modalHeader.append(buttonclose);
